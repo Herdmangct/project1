3e9a13fc227fb9a57bd40a9e642cfd57a065a7e4
Graham <herdmanctg@outlook.com>
2019-08-01 10:13:28 +1000

    added harmony

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -17,19 +17,19 @@ class IngredientsController < ApplicationController
   def create
     @ingredient = Ingredient.new ingredient_params
 
-    raise "hell"
-
-    # attach ingredient to quantities by trying to get contents of every hash
-    @current_user.meals.each do |meal|
-      quantity_attributes = params.fetch(meal.parameterize.underscore.to_sym, false)
-      quantity_attributes = quantity_attributes.split("_") unless quantity_attributes == false
-      @quantity = Quantity.new :measurement => quantity_attributes[1].to_f
-      @ingredient.quantities <<
-    end
-    @ingredient.quantities << @
-
-    @meal.save
-    redirect_to meals_path
+    # raise "hell"
+    #
+    # # attach ingredient to quantities by trying to get contents of every hash
+    # @current_user.meals.each do |meal|
+    #   quantity_attributes = params.fetch(meal.parameterize.underscore.to_sym, false)
+    #   quantity_attributes = quantity_attributes.split("_") unless quantity_attributes == false
+    #   @quantity = Quantity.new :measurement => quantity_attributes[1].to_f
+    #   @ingredient.quantities <<
+    # end
+    # @ingredient.quantities << @
+    #
+    # @meal.save
+    # redirect_to meals_path
 
     # need to catch the quantities information and create a quantity and attach it to both meal and ingredient
 
