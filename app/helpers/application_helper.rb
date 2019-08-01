4228a1f8dbc5709e2a9666f3e6989dbf83fcaba8
Graham <herdmanctg@outlook.com>
2019-08-02 09:59:57 +1000

    fixed meals table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,35 +12,4 @@ module ApplicationHelper
     end
   end
 
-  def display_standard_table(columns, collection = {})
-
-   thead = content_tag :thead do
-     content_tag :tr do
-      columns.collect {|column|  concat content_tag(:th,column[:display_name])}.join().html_safe
-     end
-   end
-
-   tbody = content_tag :tbody do
-    collection.collect { |elem|
-      content_tag :tr do
-        columns.collect { |column|
-            content_tag :td do
-              if column[:display_name] == "Meal"
-                concat link_to(elem.attributes[column[:name]], elem)
-              elsif column[:display_name] == "Sales Price" || column[:display_name] == "Cost to Make"
-                concat content_tag(:div, number_to_currency(elem.attributes[column[:name]]))
-              else
-                concat content_tag(:div, elem.attributes[column[:name]])
-              end
-            end
-
-        }.to_s.html_safe
-      end
-    }.join().html_safe
-   end
-
-   content_tag :table, thead.concat(tbody), :class => "table table-striped"
-
-  end
-
 end
